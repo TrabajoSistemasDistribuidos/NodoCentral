1abd16ccd0199a81592bb7256fafb7ae9d83259a
Samuu10 <svallmun@myuax.com>
2025-01-14 21:45:29 +0100

    Commit inicial

---
+++ src/main/java/com/proyecto/nodocentral/NodoCentralApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.nodocentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodoCentralApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NodoCentralApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/proyecto/nodocentral/NodoCentralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.nodocentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodoCentralApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
